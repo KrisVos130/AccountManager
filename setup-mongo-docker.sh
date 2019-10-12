5b6ab4435c1fae9b908f0bc68f39b0f19d886b21
Kristian Vos <krisvos130@gmail.com>
2019-10-12 13:37:37 +0200

    feat: worked on adding Docker

---
+++ setup-mongo-docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mongo musare \
+        --port ${MONGO_PORT} \
+        -u ${MONGO_ROOT_USERNAME} \
+        --authenticationDatabase "admin" \
+        -p ${MONGO_ROOT_PASSWORD} \
+        --eval "db.createUser({ user: '${MONGO_USER_USERNAME}', pwd: '${MONGO_USER_PASSWORD}', roles:[ { role:'readWrite', db: ${MONGO_DATABASE} } ] } );"
