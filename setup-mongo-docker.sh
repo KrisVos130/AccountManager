09fe36a262de2c53bd2fd8abae3e28f03a90797e
Kristian Vos <krisvos130@gmail.com>
2019-10-12 15:50:38 +0200

    fix: some more fixes for Docker in regards to Mongo initializing

--- setup-mongo-docker.sh
+++ setup-mongo-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mongo musare \
+mongo ${MONGO_DATABASE} \
         --port ${MONGO_PORT} \
         -u ${MONGO_ROOT_USERNAME} \
-        --authenticationDatabase "${MONGO_ROOT_DATABASE}" \
+        --authenticationDatabase "admin" \
         -p ${MONGO_ROOT_PASSWORD} \
         --eval "db.createUser({ user: '${MONGO_USER_USERNAME}', pwd: '${MONGO_USER_PASSWORD}', roles:[ { role:'readWrite', db: '${MONGO_DATABASE}' } ] } );"
