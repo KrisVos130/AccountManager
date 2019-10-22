7115b7856ce24b088b161fab81ae3b27625a0e88
Kristian Vos <krisvos130@gmail.com>
2019-10-22 17:14:00 +0200

    fix: fixed convertV4toV5

--- backend/schemas/convertV4toV5.js
+++ backend/schemas/convertV4toV5.js
@@ -2,12 +2,6 @@ module.exports = {
 	versionFrom: 4,
 	versionTo: 5,
 	changes: {
-		"app+appType": "newapp+newappType",
-		"app+appName": "newapp+newappName",
-		"email+email": "newemail+newemail",
-		"usesPassword+usesPassword": "newusesPassword+newusesPassword",
-		"twofaPossible+twofaPossibleType": "newtwofaPossible+newtwofaPossibleType",
-		"deletedAt+deletedAt": "newdeletedAt+newdeletedAt",
-		"createdAt+createdAt": "createdAt+newcreatedAt"
+		
 	}
 }
